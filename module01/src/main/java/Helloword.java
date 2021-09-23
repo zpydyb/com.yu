d26a048b7dfb72efdf141e20db6a077dde11eea6
asus <3379364662@qq.com>
2021-09-23 20:18:59 +0800

    Initial commit

---
+++ module01/src/main/java/Helloword.java
@@ -0,0 +1,15 @@
+import org.springframework.cglib.transform.impl.FieldProviderTransformer;
+
+import java.util.Scanner;
+
+public class Helloword {
+    public static void main(String[] args) {
+        System.out.println("helloword!!!");
+       String[] arr=new  String[]{"c","java","python"};
+        for (String s : arr) {
+            System.out.println(s);
+
+        }
+
+    }
+}
